1f2cd4610d5c8e654e78627218459d975ec1d9d0
Tarise <tsingletary@hanleywood.com>
2015-10-11 08:38:52 -0400

    refactored insertion of fake dogs to prepare to add fake parks, etc

--- pawesome.js
+++ pawesome.js
@@ -1,15 +1,17 @@
+FakeDogs = new Mongo.Collection("dogs");
+
 var paw = {
   init : function(){
-    FakeDogs = new Mongo.Collection("dogs");
+    
 
     if (Meteor.isClient) {
       // counter starts at 0
       Session.setDefault('counter', 0);
-      paw.spoofer.generateUsers(FakeDogs);
+      
 
       Template.body.helpers({
         friends: function () {
-          return FakeDogs.find({});
+          return FakeDogs.find();
         },
         mapLoaded : function(){
             return paw.map.init("paw-map");
@@ -21,6 +23,13 @@ var paw = {
       });
     }
 
+    if(Meteor.isServer){
+      Meteor.startup(function () {
+        paw.spoofer.generateUsers(FakeDogs);
+      });
+      
+    }
+
 
     Accounts.onLogin(function(){
       Meteor.users.update({_id:Meteor.user()._id}, { $set: {profile: paw.spoofer.getUserData()} });
@@ -28,89 +37,6 @@ var paw = {
     });
   },
   spoofer : {
-    fakeDogs : [
-      {
-        name: "Bo",
-        age: 7,
-        breed: "German Shepard",
-        likes: "children, strangers",
-        dislikes: "smaller dogs",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Jordan Williams" // ideally this would be an id
-      },{
-        name: "Lassie",
-        age: 1,
-        breed: "Pomeranian",
-        likes: "frisbee",
-        dislikes: "large dogs",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Alice Wrinkle" // ideally this would be an id
-      },{
-        name: "Toy",
-        age: 1,
-        breed: "Mutt",
-        likes: "children, playing catch, sleeping",
-        dislikes: "cats",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Britt Neek" // ideally this would be an id
-      },{
-        name: "Mooshoe",
-        age: 3,
-        breed: "Chow",
-        likes: "long walks, belly rubs",
-        dislikes: "squirrels",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Hank Token" // ideally this would be an id
-      },{
-        name: "Daisy",
-        age: 6,
-        breed: "German Shepard",
-        likes: "fetch, naps",
-        dislikes: "sirens, vacuums",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Tony Becker" // ideally this would be an id
-      },{
-        name: "Princess",
-        age: 12,
-        breed: "Poodle",
-        likes: "ice cubes, naps",
-        dislikes: "males, puppies",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Jason Hugh" // ideally this would be an id
-      },{
-        name: "Buddy",
-        age: 3,
-        breed: "Mutt",
-        likes: "fetch",
-        dislikes: "children",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Wilson Jefferies" // ideally this would be an id
-      },{
-        name: "Toby",
-        age: 9,
-        breed: "Golden Retriever",
-        likes: "children, food (anything)",
-        dislikes: "smaller dogs",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Morgan Freeman" // ideally this would be an id
-      },{
-        name: "Koi",
-        age: 7,
-        breed: "Pug",
-        likes: "belly rubs, cuddling",
-        dislikes: "puppies",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Seth Rogan" // ideally this would be an id
-      },{
-        name: "Ruby",
-        age: 2,
-        breed: "Golden Doodle",
-        likes: "children, fetch",
-        dislikes: "smaller dogs",
-        image: "https://openclipart.org/image/2400px/svg_to_png/166603/DogProfile.png",
-        owner: "Ashley Davidson" // ideally this would be an id
-      }
-    ],
     getUserData : function(){
       var data = Meteor.user().profile;
       if(!data){
@@ -135,8 +61,27 @@ var paw = {
       return data;
     },
     generateUsers : function(collection){
-      _.each(paw.spoofer.fakeDogs, function(dog) { 
-        collection.insert(dog);
+      var count = FakeDogs.find().count();
+
+      //console.log(count);
+
+      if (count > 0) FakeDogs.remove({}); // undoing previous code
+
+      Assets.getText('dogs.json', function(err, data) {
+        
+        var json = EJSON.parse(data);
+
+        //console.log(json);
+
+        var content = json['dogs'];
+
+        //console.log(content[0]);
+
+        for(var i = 0; i < content.length; i++){
+          //console.log(content[i]);
+          FakeDogs.insert(content[i]);
+        }
+
       });
     }
   },
